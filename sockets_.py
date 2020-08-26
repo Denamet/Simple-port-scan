b7d2f964e266216664319576764483ded01dfa2e
Denamet <65767622+Denamet@users.noreply.github.com>
2020-08-27 00:13:44 +0200

    Update sockets_.py

--- sockets_.py
+++ sockets_.py
@@ -1,12 +1,4 @@
 import socket
-# pc_name = socket.gethostname()
-# ipv4 = socket.gethostbyname(pc_name)
-# print(pc_name, ipv4)
-# host = "localhost"
-# port = 4444
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #هنا بقوله اعمل كونكشن علي السوكت الي جوه السوكت                                                         #واختار منها ايبي فيرجن فور والبورت
-# s.connect((host, port)) # هنا بقا بقلوه اعمل كونشن بالايبي والبورت الي مدهوملك فوق
-import socket
 host = input("type your ip address ")
 try:
     for port in range(1, 1000):
